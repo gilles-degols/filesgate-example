c2949f0ab907a4599595b16233f324125ef313e2
gilles-degols <gilles@degols.net>
2018-12-13 23:48:49 +0100

    Construct Source and Matcher steps

--- app/net/degols/example/fsgate/example/pipeline/Source.scala
+++ app/net/degols/example/fsgate/example/pipeline/Source.scala
@@ -1,10 +1,10 @@
 package net.degols.example.fsgate.example.pipeline
 
 import net.degols.filesgate.libs.filesgate.orm.FileMetadata
-import net.degols.filesgate.libs.filesgate.pipeline.source.{SourceApi, SourceSeed}
+import net.degols.filesgate.libs.filesgate.pipeline.datasource.{DataSourceApi, DataSourceSeed}
 
-class Source extends SourceApi{
-  override def process(sourceSeed: SourceSeed): Iterator[FileMetadata] = {
+class Source extends DataSourceApi{
+  override def process(sourceSeed: DataSourceSeed): Iterator[FileMetadata] = {
     (1 to 100).map(number => FileMetadata(s"http://localhost/img/$number")).toIterator
   }
 }
