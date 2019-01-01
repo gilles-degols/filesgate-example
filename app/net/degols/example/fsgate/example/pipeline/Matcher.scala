ed0325245e1263bb19540df9070f23a8a882db58
gilles-degols <gilles@degols.net>
2019-01-01 23:53:40 +0100

    Example to make a pipeline instance wait before processing next message

--- app/net/degols/example/fsgate/example/pipeline/Matcher.scala
+++ app/net/degols/example/fsgate/example/pipeline/Matcher.scala
@@ -1,16 +1,27 @@
 package net.degols.example.fsgate.example.pipeline
 
+import java.util.concurrent.{Callable, Executors, ScheduledExecutorService, TimeUnit}
+
+import akka.actor.{ActorContext, ActorSystem}
 import net.degols.libs.filesgate.orm.FileMetadata
 import net.degols.libs.filesgate.pipeline.matcher.{MatcherApi, MatcherMessage}
 
 import scala.concurrent.{ExecutionContext, Future}
+import akka.pattern.after
+import play.api.Logger
 
-class Matcher(implicit val ec: ExecutionContext) extends MatcherApi{
+import scala.concurrent.duration._
+class Matcher(implicit val ec: ExecutionContext, context: ActorContext) extends MatcherApi{
+  // Useful if we want to stop downloading files during a specific time without using any Await
+  val schedule: ScheduledExecutorService = Executors.newSingleThreadScheduledExecutor()
+
   /**
     * @param fileMetadata
     * @return true if the current pipeline is meant to download the file, or not.
     */
   override def process(matcherMessage: MatcherMessage): Future[MatcherMessage] = {
+    // Code below to wait a specific time before downloading the message. This will automatically "stuck" the current pipeline
+    //after(1 seconds, context.system.scheduler)(Future{matcherMessage})
     Future{matcherMessage}
   }
 }
